5647374e6aecc4ad40d2b967a1385c879febd44d
Edgar Santos <edfil221@gmail.com>
2019-04-21 09:50:22 +0200

    Initial commit with moving Capsule and Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform _target;
+	public Vector2 _defaultRotation = new Vector2(0.0f, 30.0f);
+	public float _defaultDistance = -7.0f;
+	public float _rotateSpeed = 1;
+
+	private Vector2 _currentRotation;
+	private float _currentDistance;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Cursor.lockState = CursorLockMode.Locked;
+		ResetCamera();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.R)) {
+			ResetCamera();
+		}
+
+		float horizontalAxis = Input.GetAxis("Mouse X") * _rotateSpeed;
+		float verticalAxis = Input.GetAxis("Mouse Y") * _rotateSpeed * -1;
+		float scrollAxis = Input.GetAxis("Mouse ScrollWheel") * _rotateSpeed;
+
+		_currentRotation.x += horizontalAxis;
+		_currentRotation.y += verticalAxis;
+		_currentDistance += scrollAxis;
+
+		Vector3 lookDirection = _target.forward;
+		Quaternion xRotation = Quaternion.AngleAxis(_currentRotation.x, Vector3.up);
+		Quaternion yRotation = Quaternion.AngleAxis(_currentRotation.y, Vector3.right);
+		lookDirection = xRotation * yRotation * lookDirection;
+
+		transform.position = _target.position + lookDirection * _currentDistance;
+		if (transform.position.y < _target.position.y) {
+			transform.position = new Vector3(transform.position.x, _target.position.y, transform.position.z);
+		}
+
+		transform.LookAt(_target);
+	}
+
+	private void ResetCamera()
+	{
+		_currentRotation = _defaultRotation;
+		_currentDistance = _defaultDistance;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float _moveSpeed = 1.0f;
+	public float _jumpForce = 1.0f;
+	public float _gravityScale = 1.0f;
+
+	private CharacterController _characterController;
+	private Vector3 _playerMotion = Vector3.zero;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		_characterController = GetComponent<CharacterController>();
+	}
+
+
+	// Update is called once per frame
+	void Update()
+    {
+		// Update Input
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+		float verticalAxis = Input.GetAxisRaw("Vertical");
+		bool isJumpPressed = Input.GetAxisRaw("Jump") > 0.0f;
+
+		// Update player motion
+		_playerMotion.Set(horizontalAxis * _moveSpeed, _playerMotion.y, verticalAxis * _moveSpeed);
+
+		if (_characterController.isGrounded)
+		{
+			if (isJumpPressed)
+				_playerMotion.y = _jumpForce;
+			else
+				_playerMotion.y = Physics.gravity.y * _gravityScale * Time.deltaTime;
+		} else
+		{
+			_playerMotion.y += Physics.gravity.y * _gravityScale * Time.deltaTime;
+		}
+
+		_characterController.Move(_playerMotion * Time.deltaTime);
+	}
+}
