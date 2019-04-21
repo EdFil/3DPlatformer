5c88f3f96e55bcefce1fff566b16c78fb6027781
Edgar Santos <edfil221@gmail.com>
2019-04-21 12:46:05 +0200

    Player movement based on Camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	public Transform _pivot;
 	public float _moveSpeed = 1.0f;
 	public float _jumpForce = 1.0f;
 	public float _gravityScale = 1.0f;
 
+	private Vector3 _currentMoveDirection;
 	private CharacterController _characterController;
 	private Vector3 _playerMotion = Vector3.zero;
 
@@ -26,8 +28,10 @@ public class PlayerMovement : MonoBehaviour
 		float verticalAxis = Input.GetAxisRaw("Vertical");
 		bool isJumpPressed = Input.GetAxisRaw("Jump") > 0.0f;
 
+		Vector3 movementDirection = GetMovementDirection(horizontalAxis, verticalAxis);
+
 		// Update player motion
-		_playerMotion.Set(horizontalAxis * _moveSpeed, _playerMotion.y, verticalAxis * _moveSpeed);
+		_playerMotion.Set(movementDirection.x * _moveSpeed, _playerMotion.y, movementDirection.z * _moveSpeed);
 
 		if (_characterController.isGrounded)
 		{
@@ -35,11 +39,28 @@ public class PlayerMovement : MonoBehaviour
 				_playerMotion.y = _jumpForce;
 			else
 				_playerMotion.y = Physics.gravity.y * _gravityScale * Time.deltaTime;
-		} else
+		}
+		else
 		{
 			_playerMotion.y += Physics.gravity.y * _gravityScale * Time.deltaTime;
 		}
 
 		_characterController.Move(_playerMotion * Time.deltaTime);
+	}
+
+	private Vector3 GetMovementDirection(float horizontalAxis, float verticalAxis)
+	{
+		if (horizontalAxis == 0.0f && verticalAxis == 0.0f)
+			return Vector3.zero;
+
+		// Calculate rotation angle based on Axis input
+		float angle = 0.0f;
+		if (horizontalAxis != 0.0f && verticalAxis != 0.0f) {
+			angle = 45.0f * horizontalAxis + (verticalAxis < 0.0f ? 90.0f * horizontalAxis : 0.0f);
+		} else {
+			angle = 90.0f * horizontalAxis + (verticalAxis < 0.0f ? 180.0f : 0.0f);
+		}
+
+		return Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.ProjectOnPlane(_pivot.forward, Vector3.up);
 	}
 }
