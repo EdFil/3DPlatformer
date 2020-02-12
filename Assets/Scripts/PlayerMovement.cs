21bb46f44636899602d9b87d640cb50364f23b5c
Montx <fateweaving@hotmail.com>
2020-02-12 22:24:17 +0100

    Player Motion V2 and Aiming

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,12 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController _characterController;
     private Vector3 _playerMotion = Vector3.zero;
+    private Transform _cameraTransform;
+    private bool _isPlayerMotionV2 = false;
 
     void Start() {
         _characterController = GetComponent<CharacterController>();
+        _cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
@@ -21,15 +24,20 @@ public class PlayerMovement : MonoBehaviour
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
         bool isJumpPressed = Input.GetAxisRaw("Jump") > 0.0f;
+        bool isAiming = Input.GetMouseButton(1);
 
-        // Update direction
-        Vector3 movementDirection = GetMovementDirection(horizontalAxis, verticalAxis);
-        if (movementDirection != Vector3.zero) {
-            transform.forward = movementDirection;
+        if (Input.GetKeyUp(KeyCode.F1)) {
+            _isPlayerMotionV2 = !_isPlayerMotionV2;
         }
 
-        // Update motion
-        _playerMotion.Set(movementDirection.x * _moveSpeed, _playerMotion.y, movementDirection.z * _moveSpeed);
+        if (_isPlayerMotionV2)
+        {
+            SetPlayerMotionV2(horizontalAxis, verticalAxis, isAiming);
+        }
+        else {
+            SetPlayerMotionV1(horizontalAxis, verticalAxis);
+        }
+
         if (_characterController.isGrounded) {
             if (isJumpPressed)
                 _playerMotion.y = _jumpForce;
@@ -54,5 +62,37 @@ public class PlayerMovement : MonoBehaviour
         }
 
         return Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.ProjectOnPlane(_pivot.forward, Vector3.up);
+    }
+
+    private void SetPlayerMotionV1(float horizontalAxis, float verticalAxis) {
+        // Update direction
+        Vector3 movementDirection = GetMovementDirection(horizontalAxis, verticalAxis);
+        if (movementDirection != Vector3.zero)
+        {
+            transform.forward = movementDirection;
+        }
+
+        // Update motion
+        _playerMotion.Set(movementDirection.x * _moveSpeed, _playerMotion.y, movementDirection.z * _moveSpeed);
+    }
+
+    private void SetPlayerMotionV2(float horizontalAxis, float verticalAxis, bool isAiming)
+    {
+        Vector3 inputDirection = new Vector3(horizontalAxis, 0, verticalAxis);
+
+        if (inputDirection != Vector3.zero)
+        {
+            transform.forward = new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z);
+        }
+
+        inputDirection = transform.TransformDirection(inputDirection * _moveSpeed);
+
+        if (inputDirection != Vector3.zero && !isAiming)
+        {
+            transform.forward = inputDirection;
+        }
+
+        // Update player motion
+        _playerMotion.Set(inputDirection.x, _playerMotion.y, inputDirection.z);
     }
 }
