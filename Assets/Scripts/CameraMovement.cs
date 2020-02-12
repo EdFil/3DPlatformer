c412bd2da61a092f63090f6651d1b80af8ee2dda
Edgar Santos <edfil221@gmail.com>
2020-02-12 20:06:11 +0100

    Revert back to 360 movement and convert tabs to spaces

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,57 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-	public Transform _target;
-	public float _defaultDistance = 7.0f;
-	public Vector2 _defaultRotation = new Vector2(0.0f, 20.0f);
-	public float _rotateSpeed = 1;
-	public bool _isXAxisInverted = false;
-	public bool _isYAxisInverted = false;
+    public Transform _target;
+    public float _defaultDistance = 7.0f;
+    public Vector2 _defaultRotation = new Vector2(0.0f, 20.0f);
+    public float _rotateSpeed = 1;
+    public bool _isXAxisInverted = false;
+    public bool _isYAxisInverted = false;
 
-	private Vector2 _currentRotation;
-	private float _currentDistance;
-	public LayerMask _layerMask;
+    private Vector2 _currentRotation;
+    private float _currentDistance;
+    public LayerMask _layerMask;
 
-	// Start is called before the first frame update
-	void Start()
-    {
-		Cursor.lockState = CursorLockMode.Locked;
-		ResetCamera();
-	}
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        ResetCamera();
+    }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.R)) {
-			ResetCamera();
-		}
+    void LateUpdate() {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.R)) {
+            ResetCamera();
+        }
 
-		float horizontalAxis = Input.GetAxis("Mouse X") * _rotateSpeed * (_isXAxisInverted ? 1.0f : -1.0f);
-		float verticalAxis = Input.GetAxis("Mouse Y") * _rotateSpeed * (_isYAxisInverted ? 1.0f : -1.0f);
-		float scrollAxis = Input.GetAxis("Mouse ScrollWheel") * _rotateSpeed;
+        float horizontalAxis = Input.GetAxis("Mouse X") * _rotateSpeed * (_isXAxisInverted ? 1.0f : -1.0f);
+        float verticalAxis = Input.GetAxis("Mouse Y") * _rotateSpeed * (_isYAxisInverted ? 1.0f : -1.0f);
+        float scrollAxis = Input.GetAxis("Mouse ScrollWheel") * _rotateSpeed;
 
-		_currentRotation.x = Mathf.Clamp(_currentRotation.x + verticalAxis, -10.0f, 80.0f);
-		_currentRotation.y -= horizontalAxis;
-		_currentDistance += scrollAxis;
+        _currentRotation.x = Mathf.Clamp(_currentRotation.x + verticalAxis, -10.0f, 80.0f);
+        _currentRotation.y -= horizontalAxis;
+        _currentDistance += scrollAxis;
 
-		Vector3 lookDirection = Quaternion.Euler(_currentRotation.x, _currentRotation.y, 0.0f) * Vector3.forward;
-		float distanceForCamera = _currentDistance;		
-		
-		RaycastHit hitInfo;
-		if (Physics.Raycast(_target.position, -lookDirection, out hitInfo, _currentDistance, _layerMask)) {
-			distanceForCamera = hitInfo.distance;
-		}
+        Vector3 lookDirection = Quaternion.Euler(_currentRotation.x, _currentRotation.y, 0.0f) * Vector3.forward;
+        float distanceForCamera = _currentDistance;
 
-		transform.position = _target.position - lookDirection * distanceForCamera;
-		transform.LookAt(_target);
-	}
+        RaycastHit hitInfo;
+        if (Physics.Raycast(_target.position, -lookDirection, out hitInfo, _currentDistance, _layerMask)) {
+            distanceForCamera = hitInfo.distance;
+        }
 
-	private void ResetCamera()
-	{
-		_currentRotation.Set(_defaultRotation.y, Vector3.Angle(_target.forward, Vector3.forward));
-		_currentDistance = _defaultDistance;
-	}
+        transform.position = _target.position - lookDirection * distanceForCamera;
+        transform.LookAt(_target);
+    }
+
+    private void ResetCamera() {
+        _currentRotation.Set(_defaultRotation.y, Vector3.Angle(_target.forward, Vector3.forward));
+        _currentDistance = _defaultDistance;
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,80 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Transform _cameraTransform;
-
     public Transform _pivot;
-	public float _moveSpeed = 1.0f;
-	public float _jumpForce = 1.0f;
-	public float _gravityScale = 1.0f;
-	public float _rotationSpeed;
+    public float _moveSpeed = 8.0f;
+    public float _jumpForce = 12.0f;
+    public float _gravityScale = 4.0f;
+    public float _rotationSpeed;
 
-	private Vector3 _playerDirection;
-	private CharacterController _characterController;
-	private Vector3 _playerMotion = Vector3.zero;
+    private CharacterController _characterController;
+    private Vector3 _playerMotion = Vector3.zero;
 
-	// Start is called before the first frame update
-	void Start()
-    {
-		_characterController = GetComponent<CharacterController>();
-        _cameraTransform = Camera.main.transform;
+    void Start() {
+        _characterController = GetComponent<CharacterController>();
     }
 
+    // Update is called once per frame
+    void Update() {
+        // Update Input
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+        bool isJumpPressed = Input.GetAxisRaw("Jump") > 0.0f;
 
-	// Update is called once per frame
-	void Update()
-    {
-		// Update Input
-		float horizontalAxis = Input.GetAxisRaw("Horizontal");
-		float verticalAxis = Input.GetAxisRaw("Vertical");
-		bool isJumpPressed = Input.GetAxisRaw("Jump") > 0.0f;
-
-        //Debug.DrawLine(cameraTransform.position, cameraTransform.position + cameraTransform.forward * 10, Color.green, 0.5f);
-
-        Vector3 inputDirection = new Vector3(horizontalAxis, 0, verticalAxis);
-
-        if (inputDirection != Vector3.zero)
-        {
-            transform.forward = new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z);
+        // Update direction
+        Vector3 movementDirection = GetMovementDirection(horizontalAxis, verticalAxis);
+        if (movementDirection != Vector3.zero) {
+            transform.forward = movementDirection;
         }
 
-        inputDirection = transform.TransformDirection(inputDirection * _moveSpeed);
- 
-        // Update player motion
-        _playerMotion.Set(inputDirection.x, _playerMotion.y, inputDirection.z);
-
-        if (_characterController.isGrounded)
-		{
-			if (isJumpPressed)
-				_playerMotion.y = _jumpForce;
-			else
-				_playerMotion.y = Physics.gravity.y * _gravityScale * Time.deltaTime * _moveSpeed;
-		}
-		else
-		{
-			_playerMotion.y += Physics.gravity.y * _gravityScale * Time.deltaTime;
-		}
-
+        // Update motion
+        _playerMotion.Set(movementDirection.x * _moveSpeed, _playerMotion.y, movementDirection.z * _moveSpeed);
+        if (_characterController.isGrounded) {
+            if (isJumpPressed)
+                _playerMotion.y = _jumpForce;
+            else
+                _playerMotion.y = Physics.gravity.y * _gravityScale * Time.deltaTime * _moveSpeed;
+        } else {
+            _playerMotion.y += Physics.gravity.y * _gravityScale * Time.deltaTime;
+        }
         _characterController.Move(_playerMotion * Time.deltaTime);
-
     }
 
-	/*private Vector3 GetMovementDirection(float horizontalAxis, float verticalAxis)
-	{
-		if (horizontalAxis == 0.0f && verticalAxis == 0.0f)
-			return Vector3.zero;
+    private Vector3 GetMovementDirection(float horizontalAxis, float verticalAxis) {
+        if (horizontalAxis == 0.0f && verticalAxis == 0.0f)
+            return Vector3.zero;
 
-		// Calculate rotation angle based on Axis input
-		float angle = 0.0f;
-		if (horizontalAxis != 0.0f && verticalAxis != 0.0f) {
-			angle = 45.0f * horizontalAxis + (verticalAxis < 0.0f ? 90.0f * horizontalAxis : 0.0f);
-		} else {
-			angle = 90.0f * horizontalAxis + (verticalAxis < 0.0f ? 180.0f : 0.0f);
-		}
+        // Calculate rotation angle based on Axis input
+        float angle = 0.0f;
+        if (horizontalAxis != 0.0f && verticalAxis != 0.0f) {
+            angle = 45.0f * horizontalAxis + (verticalAxis < 0.0f ? 90.0f * horizontalAxis : 0.0f);
+        } else {
+            angle = 90.0f * horizontalAxis + (verticalAxis < 0.0f ? 180.0f : 0.0f);
+        }
 
-		return (Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.ProjectOnPlane(_pivot.forward, Vector3.up)).normalized;
-	}*/
+        return Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.ProjectOnPlane(_pivot.forward, Vector3.up);
+    }
 }
